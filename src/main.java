86ccc98c122fb3699d06c5eaa28655d731fd19b8
SuchGlasha <such.glasha@gmail.com>
2016-04-01 10:33:14 +0300

    исправлены явные ошибки в main

--- src/main.java
+++ src/main.java
@@ -1,16 +1,23 @@
 /**
  * Created by Student on 01.04.2016.
  */
-import jdk.nashorn.internal.runtime.NumberToString;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class main {
 
     public static void main(String[] args) {
-        string s = "";
-        Scanner in = new Scanner(new File(/*way*/));
+        String s = "";
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("C://Users//Student//IdeaProjects//regexp_lab//text.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         while(in.hasNext())
             s += in.nextLine() + "\r\n";
         in.close();
@@ -20,8 +27,8 @@ public class Main {
         while (m.find())
         {
             long number = Long.parseLong(m.group());
-            if (number < 100) m.replaceAll(NumberToString.convertLessThanOneThousand((int) number));
-            else m.replaceAll(NumberToString.convert(number));
+            if (number < 100) m.replaceAll(NumbersToWords.convertLessThanOneThousand((int) number));
+            else m.replaceAll(NumbersToWords.convert(number));
         }
     }
 }
